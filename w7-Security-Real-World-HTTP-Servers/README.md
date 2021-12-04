8bf8fd85dad3301471f375e20b76f6d4590aaaf9
Sandeep <sandeepchopra7@gmail.com>
2021-12-04 17:20:40 -0500

    added notes and complete code

--- w7-Security-Real-World-HTTP-Servers/README.md
+++ w7-Security-Real-World-HTTP-Servers/README.md
@@ -1,3 +1,46 @@
 # Lecture - Security and Real World HTTP Servers
 
+Lecture Outline
 
+Slides:
+
+👋🏻 Thanks for coming to the lecture. Today we discussed about Security and real world HTTP Servers:
+
+We talked about:
+
+Security issue #1 - Passwords save in plain text can be a huge security risk!
+Solution: Hashing (for e.g. using BcryptJS)
+
+
+Security issue #2 - Cookies save in plain text can be a huge security risk!
+Solution: Encryption (for e.g. using cookie-session instead of cookie-parser)
+
+
+Security issue #3 - Cookies can be stolen
+Solution: https 
+
+Man in the middle attack - how they work and how to avoid it.
+
+We did quick overview of the following as well:
+
+1. Restful API conventions
+2. CRUD & REST conventions
+3. Creating routes for our APP (for our previous spacex repo)
+
+We also discussed about the middlewares and how they work. 
+
+
+Demo App
+
+We coded together a SpaceX demo app which has dashboard to launch rockets 🚀
+
+> We implemented login, logout functionality using cookie-parser and plain text passwords
+> We protected our `/dashboard` and `/launch` routes form the unauthorized passwords
+> We then switched from cookie-parser to cookie-session to encrypt our cookies
+> We also used bcryptjs to hash our passwords using salts
+>  [Bonus] As a stretch after the main lecture, we made our own middleware to protect out routets 
+
+Some useful resources:
+1. https://www.npmjs.com/package/cookie-parser
+2. https://www.npmjs.com/package/bcrypt
+3. https://expressjs.com/en/guide/writing-middleware.html
--- w7-Security-Real-World-HTTP-Servers/app.js
+++ w7-Security-Real-World-HTTP-Servers/app.js
@@ -3,6 +3,11 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
 
 const PORT = 3000;
 
@@ -14,8 +19,157 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['random keys orange', 'random keys apple']
+  })
+);
+
+const user1 = {
+  name: 'Tim Berners Lee',
+  email: 'tim@cern.eu',
+  password: bcrypt.hashSync('password1', salt)
+};
+
+const user2 = {
+  name: 'Lady Ada Lovelace',
+  email: 'ada@lovelace',
+  password: bcrypt.hashSync('password2', salt)
+};
+
+console.log({user1})
+
+const database = {
+  'tim@cern.eu': user1,
+  'ada@lovelace': user2
+};
+
+
+const protectRoutesMiddleWare = (req, res, next) => {
+const { email } = req.session;
+  console.log({email})
+const currentUser = database[email];
+
+if(!currentUser) {
+  res.redirect('/')
+} else {
+  next()
+}
+}
+
+// app.use(protectRoutesMiddleWare);
+
+const authenticateUser = (email, password) => {
+  let error;
+  let user;
+
+  const currentUser = database[email];
+
+
+  // Old method
+  // const isPasswordInValid = currentUser.password !== password;
+  // New method using bcrypt
+  //This will return true if password match
+  // or else will return false if they do not
+
+   const isPasswordValid =  bcrypt.compareSync(password, currentUser.password);
+
+
+
+  if (!currentUser) {
+    error = 'The user does not exist.';
+  } else {
+    if (!isPasswordValid) {
+      error = 'The passwords do not match.';
+    }
+  } 
+  if (!error) {
+    return {
+      error: null,
+      user: currentUser
+    };
+  } else {
+    return {
+      error: error,
+      user: null
+    };
+  }
+};
+
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const {user, error} = authenticateUser(email, password);
+
+  if(error) {
+    res.send(error);
+  } else {
+    // res.cookie('email', email)
+    req.session.email = email;
+    res.redirect('/dashboard')
+  }
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+
+app.post('/register', (req, res) => {
+  const { email, password, name } = req.body;
+
+  // Create new user our in our database
+  database[email] = {
+    email,
+    password: bcrypt.hashSync(password, salt),
+    name
+  };
+
+  console.log(database[email])
+
+  // res.cookie('email', email);
+  req.session.email = email;
+  res.redirect('/dashboard')
+});
+
+app.post('/logout', (req, res) => {
+   res.clearCookie('session');
+  // res.clearCookie('email');
+  // req.session = null;
+  // delete req.session.email;
+  res.redirect('/')
+});
+
+app.get('/dashboard',protectRoutesMiddleWare, (req, res) => {
+  // const {email} = req.session;
+  // // const { email } = req.cookies;
+  // if(email) {
+  // res.render('dashboard');
+  // } else {
+  //   res.redirect('/login')
+  // }
+   res.render('dashboard');
+
+});
+
+app.get('/launch', protectRoutesMiddleWare, (req, res) => {
+  // const { email } = req.session;
+  //   // const { email } = req.cookies;
+  // if(email) {
+  // res.render('launch');
+  // } else {
+  //   res.redirect('/login')
+  // }
+
+    res.render('launch');
 });
 
 app.listen(PORT, () => {
